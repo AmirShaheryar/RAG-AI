31fa8ef2aff6ec267738c37ff8cbb08e536ecb0f
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-21 21:41:42 +0500

    Text Reader Half Completed

---
+++ DocReader/TextReader.py
@@ -0,0 +1,63 @@
+#############################################################################################
+
+######################################LangcHAIN COMMUNITY Text Reader########################
+
+#############################################################################################
+
+from langchain_community.document_loaders import TextLoader
+
+file=TextLoader("AI.txt",encoding="utf-8")
+
+docs=file.load()
+
+print(docs)
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+                
+
+
+#############################################################################################
+
+######################################LangcHAIN COMMUNITY PDFReader##########################
+
+#############################################################################################
+
+
+
+from langchain_community.document_loaders import PyPDFLoader
+
+loader=PyPDFLoader("AI_Healthcare_Guide.pdf")
+
+docs=loader.load()
+
+
+print(docs)
+
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+
+#############################################################################################
+
+######################################LangcHAIN COMMUNITY CSVReader##########################
+
+#############################################################################################
+
+
+from langchain_community.document_loaders import CSVLoader
+
+loader=CSVLoader("Healthcare_Patients.csv")
+
+docs = loader.load()
+
+print(len(docs))
+
+
+print(docs[0].metadata)
+
+print(docs[0].page_content)
