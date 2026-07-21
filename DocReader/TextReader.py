588d8e9a676776b7feda2408e868b4f2e34415fd
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-21 21:49:19 +0500

    URL Also Added

--- DocReader/TextReader.py
+++ DocReader/TextReader.py
@@ -61,3 +61,24 @@ print(len(docs))
 print(docs[0].metadata)
 
 print(docs[0].page_content)
+
+
+
+#############################################################################################
+
+######################################LangcHAIN COMMUNITY URL################################
+
+#############################################################################################
+
+
+from langchain_community.document_loaders import WebBaseLoader
+
+url="https://en.wikipedia.org/wiki/Artificial_intelligence"
+
+loader=WebBaseLoader(url)
+
+docs=loader.load()
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
