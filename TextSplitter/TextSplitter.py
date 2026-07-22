67dfcc0d78015ec43c7e746db89f64e1d1e2f757
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-22 12:50:47 +0500

    TextSplitter with DOC First with Character Split

--- TextSplitter/TextSplitter.py
+++ TextSplitter/TextSplitter.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-
+from langchain_text_splitters import CharacterTextSplitter
 
 loader=PyPDFLoader("AI_Healthcare_Guide.pdf")
 
@@ -8,3 +8,16 @@ docs=loader.load()
 
 print(docs[0].page_content)
 
+text_split=CharacterTextSplitter(
+    chunk_size=100, 
+    chunk_overlap=0,
+    separator="\n"
+    )
+
+docs_split=text_split.split_documents(docs)
+
+print(docs_split[0].page_content)
+
+print(len(docs_split))
+
+print(docs_split)
