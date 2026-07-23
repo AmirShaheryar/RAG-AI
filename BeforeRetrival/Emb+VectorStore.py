8ff9754bea0a7b4ef3ef94300bb6b2a7330f11f0
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-23 20:42:25 +0500

    Split Done

--- BeforeRetrival/Emb+VectorStore.py
+++ BeforeRetrival/Emb+VectorStore.py
@@ -14,3 +14,15 @@ document=loader.load()
 print(document[0].page_content)
 
 
+print("Splitting Document into chunks...")
+
+text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=100, 
+    chunk_overlap=20,
+    separators=["\n\n","\n"," ",""]
+    )
+
+chunks=text_splitter.split_documents(document)
+
+print("len(chunks): ", len(chunks))
+print("First chunk: ", chunks[0].page_content)
