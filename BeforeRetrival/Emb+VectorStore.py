f2d5ee73fd457f6807991e36a50377026e76e6cc
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-23 20:37:14 +0500

    DOC DONE

--- BeforeRetrival/Emb+VectorStore.py
+++ BeforeRetrival/Emb+VectorStore.py
@@ -3,3 +3,14 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
+
+
+print("Loading PDF document... ")
+
+loader=PyPDFLoader("AI_Healthcare_Guide.pdf")
+
+document=loader.load()
+
+print(document[0].page_content)
+
+
