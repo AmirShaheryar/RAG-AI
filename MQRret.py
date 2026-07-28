14bab3bc17d0cf379941a2436b77bca94e30691b
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-07-28 20:16:55 +0500

    MQR BASE Doc Loaded and Split

--- MQRret.py
+++ MQRret.py
@@ -1,7 +1,18 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 docs=PyPDFLoader("RAG_MQR_Contextual_Retrieval_Test_Document.pdf").load()
 
 print(docs[0].page_content)
 
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100, 
+    chunk_overlap=20,
+    separators=["\n\n","\n"," ",""]
+    )
 
+split_Doc=splitter.split_documents(docs)
+
+
+print(split_Doc[0].page_content)
